2b7e3175e3decd3a51be40396d15c313475d7091
HellRaiseR <deathbringer19@mail.ru>
2020-04-09 21:45:38 +0400

    Complete SimpleBigNumbersService

--- src/main/java/com/epam/izh/rd/online/service/SimpleBigNumbersService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleBigNumbersService.java
@@ -2,18 +2,22 @@ package com.epam.izh.rd.online.service;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 
 public class SimpleBigNumbersService implements BigNumbersService {
 
     /**
      * Метод делит первое число на второе с заданной точностью
      * Например 1/3 с точностью 2 = 0.33
+     *
      * @param range точность
      * @return результат
      */
     @Override
     public BigDecimal getPrecisionNumber(int a, int b, int range) {
-        return null;
+        BigDecimal bigDecimalA = BigDecimal.valueOf(a);
+        BigDecimal bigDecimalB = BigDecimal.valueOf(b);
+        return bigDecimalA.divide(bigDecimalB, range, RoundingMode.HALF_UP);
     }
 
     /**
@@ -24,6 +28,20 @@ public class SimpleBigNumbersService implements BigNumbersService {
      */
     @Override
     public BigInteger getPrimaryNumber(int range) {
-        return null;
+        int simpleDigit = 2;
+        int count = 1;
+        while (count <= range) {
+            simpleDigit++;
+            int j = 0;
+            for (int i = 1; i <= simpleDigit; i++) {
+                if ((simpleDigit % i) == 0) {
+                    j++;
+                }
+            }
+            if (j == 2) {
+                count++;
+            }
+        }
+        return BigInteger.valueOf(simpleDigit);
     }
 }
